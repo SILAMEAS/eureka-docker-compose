d120903aeffb5ab7c91dfae6d3121cfc1f4dec59
DESKTOP-GQAGB7I\ASUS <silam8422@gmail.com>
2025-06-28 18:28:15 +0700

    working local

--- product/src/main/java/com/test/product/ProductController.java
+++ product/src/main/java/com/test/product/ProductController.java
@@ -1,6 +1,7 @@
 package com.test.product;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,8 +12,11 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
-
     @GetMapping
+    public ResponseEntity<String> getAll() {
+        return ResponseEntity.ok("Products work!");
+    }
+    @GetMapping("/jkl")
     public List<Product> getAllProducts() {
         return productService.getAllProducts();
     }
